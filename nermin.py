eb1d0adc2f91e5838272032efc2c05e9d4f7ad43
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-11-30 00:24:10 +0400

    Update nermin.py

--- nermin.py
+++ nermin.py
@@ -16,7 +16,7 @@ import random
 @Nermin.on(events.NewMessage)
 async def yeni_mesaj(event: events.NewMessage.Event):
     mesaj = str(event.raw_text)
-    if mesaj == "salam":
+    if mesaj == f"{random.choice(salam)}":
       await event.reply(f"{random.choice(salam)}")
 
 @Nermin.on(events.NewMessage)
