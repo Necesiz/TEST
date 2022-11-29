a3143b2a2e8416ef15810dbcca4be262fac6f279
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-11-29 23:41:54 +0400

    Create aykhan.py

---
+++ komekci/aykhan.py
@@ -0,0 +1,11 @@
+from telethon import TelegramClient
+
+Config məlumatları
+
+# Telegram Client (Telethon)
+API_ID = 16723398
+API_HASH = "9e07dd89d2f39bfadfd59798705e4662"
+bot_token = "5760893601:AAEs4y2pNdBBhIzzET9TGLmfWl9AxbP7ZZw"
+
+# KiberAz
+Nermin = TelegramClient('client', API_ID, API_HASH).start(bot_token=bot_token)
