b1516d9aa0a91b630bb125c7975e51fed571c2ec
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-11-30 00:19:01 +0400

    Update nermin.py

--- nermin.py
+++ nermin.py
@@ -8,7 +8,7 @@
 
 
 from komekci.aykhan import Nermin
-from mesajlar.mesaj import salam
+from mesajlar.mesaj import salam, necesen, sagol
 from telethon import events, Button
 import random
 
@@ -18,6 +18,20 @@ async def yeni_mesaj(event: events.NewMessage.Event):
     mesaj = str(event.raw_text)
     if mesaj == "salam":
       await event.reply(f"{random.choice(salam)}")
+
+@Nermin.on(events.NewMessage)
+async def yeni_mesaj(event: events.NewMessage.Event):
+    mesaj = str(event.raw_text)
+    if mesaj == "necəsən":
+      await event.reply(f"{random.choice(necesen)}")
+
+@Nermin.on(events.NewMessage)
+async def yeni_mesaj(event: events.NewMessage.Event):
+    mesaj = str(event.raw_text)
+    if mesaj == "sağol":
+      await event.reply(f"{random.choice(sagol)}")
+
+
 
 print(">> Nermin qoz kimi işləyir ♿ @RoBotlarimTg @aykhan_s <<")
 Nermin.run_until_disconnected()
