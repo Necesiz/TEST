bd5cc0857ccc22cdd7c95153241ef7fc11f622fd
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-11-29 23:46:55 +0400

    Create nermin.py

---
+++ nermin.py
@@ -0,0 +1,6 @@
+# Bu repo aykhan_s tərəfindən 29.11.2022 tarixində yığılıb
+# Bu repodan icazəsiz hər hansı kodu sətri məlumatı 
+# İcazəsiz kopyalıyan öz adına çıxaran işlədən peysərdi.
+
+# t.me/RoBotlarimTg | YouTube: RoBotlarimTg |
+# t.mr/aykhan_s | insta: aykhan026 | 
