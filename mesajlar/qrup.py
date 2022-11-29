c4cb7d8adaa68a0f4421ecf9e38a7a1e09ba633c
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-11-30 02:42:30 +0400

    Update qrup.py

--- mesajlar/qrup.py
+++ mesajlar/qrup.py
@@ -5,3 +5,11 @@ yeni_user = (
 "Səni gözləməydən gözüm qaralıbe gəl çıxdaa",
 "Xoşsann Ürəyimm ❤️",
 )
+
+start = (
+"Gəldi başımın bəlası 😂❤️",
+"Nədi ba dinc duranmadın ? 😂",
+"Gözdüyürsən deyimki xoş gəldin ? 😂",
+"Ay bala mucuxlamasan olmur ? 😂",
+"Yaman bekarsan ha öz aramızdı 😂",
+)
