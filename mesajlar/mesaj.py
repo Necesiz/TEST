865e662936a02da8a3b31e5cf709d0843d39ceaf
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-11-30 00:17:07 +0400

    Update mesaj.py

--- mesajlar/mesaj.py
+++ mesajlar/mesaj.py
@@ -7,4 +7,25 @@ salam = (
 "Salamdaa",
 "Uşş balama salam",
 "Salam Cənab 🫶",
+"Salam Lələ 🔥",
+)
+
+necesen = (
+"Saol",
+"Həkimsən ?",
+"Ə belədana 😂",
+"What",
+"İyyim aşkım sen ?",
+"yaxşı olmağa çalışıram",
+"Mən başımı buraxe sən necəsən 😂",
+)
+
+sagol = (
+"Salam Sağol",
+"Hara gedsən",
+"Yatıram demə🥲",
+"Sağolunnn yenə gözləyəriyy🙈",
+"Uşş balam Sağol",
+"Sağol canım benim 🫶",
+"Sağol Kişi 🔥",
 )
