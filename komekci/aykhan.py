aeaa6de401c9ce1ca2f71913b8b4ef043205df00
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-11-30 23:50:57 +0400

    Update aykhan.py

--- komekci/aykhan.py
+++ komekci/aykhan.py
@@ -1,3 +1,13 @@
+# Bu repo aykhan_s tərəfindən 29.11.2022 tarixində yığılıb
+# Bu repodan icazəsiz hər hansı kodu sətri məlumatı kopyalıyıb
+# Öz adına çıxaran peysərdi
+# Bu yazıları silmədən işlədin
+
+# t.me/RoBotlarimTg | YouTube: RoBotlarimTg |
+# t.me/aykhan_s | insta: aykhan026 | 
+# GitHub: aykhan026
+
+
 from telethon import TelegramClient
 
 # Config məlumatları
