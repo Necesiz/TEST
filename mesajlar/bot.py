8c9a720c9b790e74ad854f43b06d98091b0db0b7
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-11-30 23:51:35 +0400

    Update bot.py

--- mesajlar/bot.py
+++ mesajlar/bot.py
@@ -1,4 +1,11 @@
-# Kopyalama oğul
+# Bu repo aykhan_s tərəfindən 29.11.2022 tarixində yığılıb
+# Bu repodan icazəsiz hər hansı kodu sətri məlumatı kopyalıyıb
+# Öz adına çıxaran peysərdi
+# Bu yazıları silmədən işlədin
+
+# t.me/RoBotlarimTg | YouTube: RoBotlarimTg |
+# t.me/aykhan_s | insta: aykhan026 | 
+# GitHub: aykhan026
 
 yeni_user = (
 "Xoş Gəldin Balam 😍",
