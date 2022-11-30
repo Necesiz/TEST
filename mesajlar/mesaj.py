463db53fcea684b71e521bef506d683ae1581f90
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-11-30 23:51:56 +0400

    Update mesaj.py

--- mesajlar/mesaj.py
+++ mesajlar/mesaj.py
@@ -1,4 +1,11 @@
-# Kopyalama oğul
+# Bu repo aykhan_s tərəfindən 29.11.2022 tarixində yığılıb
+# Bu repodan icazəsiz hər hansı kodu sətri məlumatı kopyalıyıb
+# Öz adına çıxaran peysərdi
+# Bu yazıları silmədən işlədin
+
+# t.me/RoBotlarimTg | YouTube: RoBotlarimTg |
+# t.me/aykhan_s | insta: aykhan026 | 
+# GitHub: aykhan026
 
 salam = (
 "Salam",
