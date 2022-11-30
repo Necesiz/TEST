18b4e9eea29710ed757fcb3424b036fecd0b1774
Aykhan026 <78858146+aykhan026@users.noreply.github.com>
2022-11-30 23:36:53 +0400

    Update nermin.py

--- nermin.py
+++ nermin.py
@@ -1,6 +1,7 @@
 # Bu repo aykhan_s tərəfindən 29.11.2022 tarixində yığılıb
-# Bu repodan icazəsiz hər hansı kodu sətri məlumatı 
-# İcazəsiz kopyalıyan öz adına çıxaran işlədən peysərdi.
+# Bu repodan icazəsiz hər hansı kodu sətri məlumatı kopyalıyıb
+# Öz adına çıxaran peysərdi
+# Bu yazıları silmədən işlədin
 
 # t.me/RoBotlarimTg | YouTube: RoBotlarimTg |
 # t.mr/aykhan_s | insta: aykhan026 | 
