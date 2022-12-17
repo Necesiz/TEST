4c760919c4d181c66bd5b43ce17f554fb3dd72fc
Necesiz <97000998+Necesiz@users.noreply.github.com>
2022-12-17 12:45:28 +0400

    Update nermin.py

--- nermin.py
+++ nermin.py
@@ -10,7 +10,7 @@
 
 from komekci.aykhan import Nermin
 import base64
-from mesajlar.mesaj import salam, necesen, sagol, getdim, geldim, bot, ban, hardasan, ne edirsen, sahibin kim, sehidler
+from mesajlar.mesaj import salam, necesen, sagol, getdim, geldim, bot, ban, hardasan, ne_edirsen, sahibin_kim, sehidler
 from mesajlar.bot import yeni_user, start
 from telethon import events, Button
 import random
